098b57481e0c56ddb7c3abb44c09f398d215ea80
Romina Velarde <rominapaola.velarde@gmail.com>
2020-05-26 22:49:03 +0000

    Automatically backed up by Learn

---
+++ lib/current_player.rb
@@ -0,0 +1,17 @@
+def turn_count(board)
+
+turn_count = []
+board.each do |count|
+turn_count << board 
+
+end
+turn_count
+end
+ 
+   
+def current_player(board)
+7 % 3  
+puts current_player(board)
+current_player.even? 
+turn_count(board)  
+end
